ba418b80856def787fabe868bdffc50ef4cdb0f8
Aryan <aryannikill123@gmail.com>
2025-08-08 21:25:00 +0530

    BinarySearchTree.java file added

---
+++ BinaryTree/BinarySearchTree.java
@@ -0,0 +1,89 @@
+public class BinarySearchTree {
+    
+    private Node root;
+
+    public BinarySearchTree() {}
+
+    public int height(Node node) {
+        if (node == null) {
+            return -1; // Height of an empty tree is -1
+        }
+
+        return node.height;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void populate(int[] nums) {
+        for (int num : nums) {
+            insert(num);
+        }
+    }
+
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+
+    private Node insert(int value, Node node) {
+        if (node == null) {
+            Node newNode = new Node(value);
+            return newNode;
+        }
+
+        if (value < node.getValue()) {
+            node.leftNode = insert(value, node.leftNode);
+        }
+
+        if (value > node.getValue()) {
+            node.rightNode = insert(value, node.rightNode);
+        }
+
+        node.height = Math.max(height(node.leftNode), height(node.rightNode)) + 1;
+
+        return node;
+    }
+
+    public boolean balanced(Node node) {
+        if (node == null) {
+            return true; // An empty tree is balanced
+        }
+
+        int leftHeight = height(node.leftNode);
+        int rightHeight = height(node.rightNode);
+    
+        return Math.abs(leftHeight - rightHeight) <= 1 
+            && balanced(node.leftNode) && balanced(node.rightNode);
+    }
+ 
+    public void display() {
+        display(root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(details + node.getValue());
+
+        display(node.leftNode, "Left child of " + node.getValue() + ": ");
+        display(node.rightNode, "Right child of " + node.getValue() + ": ");
+    }
+
+    public class Node {
+        private int value;
+        private Node leftNode;
+        private Node rightNode;
+        private int height;
+    
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+}
--- BinaryTree/Main.java
+++ BinaryTree/Main.java
@@ -1,9 +1,15 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        tree.populate(new Scanner(System.in));
-        tree.display();
+
+        // BinaryTree tree = new BinaryTree();
+        // tree.populate(new Scanner(System.in));
+        // tree.display();
+
+        BinarySearchTree bst = new BinarySearchTree();
+        int[] nums = {5, 3, 7, 2, 4, 6, 8};
+        bst.populate(nums);
+        bst.display();
     }
 }
